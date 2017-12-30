962fee8026705444df976141f7c622a0b7178d72
trafort <34495933+trafort@users.noreply.github.com>
2017-12-29 20:30:51 -0500

    Update main.cs

--- file/main.cs
+++ file/main.cs
@@ -7,3 +7,28 @@ if (input == "hello")
 { Console.WriteLine("Hello User! Thanks for using this operating system!");
 
 }
+
+if (input == "about")
+
+{
+
+Console.WriteLine("This command Line Os, was made by Trafort.");
+  
+Console.WriteLine("The Source code can be found right here (https://github.com/trafort/Talent/talent.git);
+
+}
+
+if (input == "help") { Console.WriteLine("hello - Simple OS command"); Console.WriteLine("about - Know about OS "); }
+
+if (input == "shutdown") { Deboot.ShutDown(); }
+
+if (input == "restart") { Deboot.Reboot(); }
+ 
+default: { Console.WriteLine("No such command");
+
+command = null; break; } }
+
+}
+
+
+
