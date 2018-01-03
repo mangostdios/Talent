ceaccaa4100f90a30d4cbbe9583a74d19c8a8664
trafort <34495933+trafort@users.noreply.github.com>
2018-01-02 19:06:44 -0500

    Add files via upload

---
+++ talent.cs
@@ -0,0 +1,19 @@
+string input;
+
+input = Console.ReadLine();
+
+if (input == "hello")
+
+{ Console.WriteLine("Hello User! Thanks for using this operating system!");
+
+}
+
+if (input == "about")
+
+{
+
+Console.WriteLine("This command Line Os, was made by Trafort.");
+  
+Console.WriteLine("The Source code can be found right here (https://github.com/trafort/Talent/talent.git);
+}
+if (input == "help") { Console.WriteLine("hello - Simple OS command"); Console.WriteLine("about - Know about OS "); }
